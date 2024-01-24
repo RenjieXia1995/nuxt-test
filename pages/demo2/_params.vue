0a1417fd3b53b24425dded3630ba2ab97d04aca9
xiarenjie <xiarenjie@ulearning.cn>
2024-01-24 17:00:14 +0800

    新增了一些demo

---
+++ pages/demo2/_params.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>这是demo2 它携带的参数是{{params}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            params:this.$route.params.params
+
+        }
+    },
+}
+</script>
---
+++ pages/demo3/demo3/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        这是demo3的子路由，接收的参数是{{ params }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            params:this.$route.params.id
+        }
+    },
+}
+</script>
